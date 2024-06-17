75282a9e01de31ae1bdc6214d88bbb775534a97f
Kajal Shukla <kajals@infinite.com>
2024-06-16 14:53:52 -1200

    addition,subtraction,multiplication,division

---
+++ CSHARP/program5/program5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int addition = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            int div = num1 / num2;
+            Console.WriteLine("addition of two numbers is:{0}", addition);
+            Console.WriteLine("subtraction of two numbers is:{0}", sub);
+            Console.WriteLine("multiplication of two numbers is:{0}", mul);
+            Console.WriteLine("division of two numbers is:{0}", div);
+            Console.Read();
+
+        }
+    }
+}
