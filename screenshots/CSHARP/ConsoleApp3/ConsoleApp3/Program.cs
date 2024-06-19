74e74851470b44087d395925b647970cb7baf36d
Kajal Shukla <kajals@infinite.com>
2024-06-19 11:43:05 -1200

    Screenshots

---
+++ screenshots/CSHARP/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first value");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second value");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("number is equal");
+            }
+            else
+            {
+                Console.WriteLine("number is not equal");
+            }
+            Console.Read();
+
+        }
+    }
+}
