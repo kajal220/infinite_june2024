83e5f56d3a7df118def8df57bb49757138ee3463
kajals <kajals@DESKTOP-8NDL4JR>
2024-06-20 23:22:28 -1200

    Assignment1

---
+++ CSHARP/Assignment/Assignment1/Assignment1/Greater_than_less_than.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Greater_than_less_than
+    {
+    public static void Test()
+    {
+        Console.Write("Enter the First Integer: ");
+        int firstnumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter the Second Integer: ");
+        int secondnumber = int.Parse(Console.ReadLine());
+
+        if (firstnumber == secondnumber)
+        {
+            Console.WriteLine("Two integers are equal");
+        }
+        else
+        {
+            Console.WriteLine("Two integers are not equal");
+        }
+        Console.Read();
+    }
+    }
+
